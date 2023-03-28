fce19769fa44df2fdd107a8b51de5f12888f9c4e
fathima <fathimabasheerr@gmail.com>
2023-03-28 15:37:19 +0530

    web

---
+++ crop webapp/website/index/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+     return render(request,'home.html')
+
+def products(request):
+     return render(request,'products.html')
+
+def recommend(request):
+     return render(request,"recommend.html")
+
+def contact(request):
+     return render(request,'contact.html')
+
+def about(request):
+     return render(request,'about.html')
